32243546e208e599e77680711010e8e673197260
guilherj <contato.guilhermenascimento@hotmail.com>
2022-01-23 10:59:27 -0300

    Projeto Iniciado

---
+++ src/main/java/com/udemy/course/CourseNelioAlvesUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseNelioAlvesUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseNelioAlvesUdemyApplication.class, args);
+	}
+
+}
