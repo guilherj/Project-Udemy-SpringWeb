d9c72fa8daba90e95ffe07c5ab5239054070a092
guilherj <contato.guilhermenascimento@hotmail.com>
2022-01-27 21:53:44 -0300

    Criado Camada Service e implementado o registro de componentes

--- src/main/java/com/udemy/course/controller/UserController.java
+++ src/main/java/com/udemy/course/controller/UserController.java
@@ -1,21 +1,37 @@
 package com.udemy.course.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.course.entities.User;
+import com.udemy.course.services.UserService;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L,"Guilherme", "guilherme@gmail.com", "8888", "123");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll(){
 		
+		List<User> list = service.findAll();		
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
